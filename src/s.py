bac149f65c248399f98d9b30e834ced69bdecaa3
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-05 00:02:51 +0530

    cli: add version and help commands

--- src/s.py
+++ src/s.py
@@ -1,5 +1,19 @@
+import sys
+
+VERSION = '0.1.0'
 def main():
-    print("s got started!!")
+    # cli args
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        print("s",VERSION)
+        print("for help:")
+        print(" s help")
+    elif len(args) == 1:
+        if args[0] == "help":
+            print("s commands:")
+            print(" 1. s      -> show version")
+            print(" 2. s help -> show this list")
 
 
 if __name__ == "__main__":
