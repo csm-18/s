7abb933fdf30d70a726ba4d2b4d9b3aa29940308
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-05 00:09:20 +0530

    cli: add compile command

--- src/s.py
+++ src/s.py
@@ -12,9 +12,12 @@ def main():
     elif len(args) == 1:
         if args[0] == "help":
             print("s commands:")
-            print(" 1. s      -> show version")
-            print(" 2. s help -> show this list")
-
+            print(" 1. s                       -> show version")
+            print(" 2. s help                  -> show this list")
+            print(" 3. s compile <filename.s>  -> compile .s file to .c file")
+    elif len(args) == 2:
+        if args[0] == "compile" and args[1][-2:] == ".s":
+            print("compiling...")
 
 if __name__ == "__main__":
     main()
