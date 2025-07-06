812843dce7ee58e3af7317900ece5649e1159866
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-07 05:12:30 +0530

    identify string token in lexical analysis

--- src/compiler/lexer.py
+++ src/compiler/lexer.py
@@ -1,3 +1,21 @@
+
+# Token Types
+token_types = {
+    "string":1,
+    "error":2,
+}
+
+
+class Token:
+    def __init__(self, type: int, value: str, start_index: int):
+        self.type = type
+        self.value = value
+        self.start_index = start_index
+
+    def __repr__(self):
+        return f"Token(type={self.type}, value='{self.value}', start_index={self.start_index})"
+
+
 def lexer(code:str):
     tokens = []
 
@@ -18,6 +36,25 @@ def lexer(code:str):
                 continue
             else:
                 break    
+        elif(code[x] == '"'):
+            found_end_quote = False
+
+            y = x+1
+            while(y < len(code)):
+                if(code[y] == '"'):
+                    found_end_quote = True
+                    break
+                y+=1
+
+            if(found_end_quote):
+                temp = Token(type=token_types["string"],value=code[x:y+1],start_index=x)
+                tokens.append(temp)
+                x = y+1
+                continue
+            else:
+                temp = Token(type=token_types["error"],value="Unclosed string literal",start_index=x)
+                tokens.append(temp)
+                break
 
         x += 1
 
