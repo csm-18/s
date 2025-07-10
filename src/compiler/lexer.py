c7eca2c18a5999fa9f00f0966e5dc0c9f854bab3
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-10 12:36:18 +0530

    lexer: add (,),{,},identifier token types to lexical analysis

--- src/compiler/lexer.py
+++ src/compiler/lexer.py
@@ -1,10 +1,33 @@
 
+
+
 # Token Types
 token_types = {
     "string":1,
     "error":2,
+    "left_paren":3,
+    "right_paren":4,
+    "left_brace":5,
+    "right_brace":6,
+    "identifier":7,
 }
 
+number_symbols = ['0','1','2','3','4','5','6','7','8','9','.']
+identifier_symbols = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                      '_''0','1','2','3','4','5','6','7','8','9','.']
+def is_identifier_symbol(text:str)->bool:
+    for char in text:
+        valid = False
+        for sym in identifier_symbols:
+            if char == sym:
+                valid = True
+                break
+        
+        if not valid:
+            return False
+
+    return True
 
 class Token:
     def __init__(self, type: int, value: str, start_index: int):
@@ -14,6 +37,8 @@ class Token:
 
     def __repr__(self):
         return f"Token(type={self.type}, value='{self.value}', start_index={self.start_index})"
+
+
 
 
 def lexer(code:str):
@@ -52,9 +77,46 @@ def lexer(code:str):
                 x = y+1
                 continue
             else:
-                temp = Token(type=token_types["error"],value="Unclosed string literal",start_index=x)
+                temp = Token(type=token_types["error"],value="Unclosed string literal!",start_index=x)
                 tokens.append(temp)
                 break
+        elif(code[x] == '('):
+            temp = Token(type=token_types["left_paren"],value=code[x],start_index=x)
+            tokens.append(temp)
+            x = x+1
+            continue
+        elif(code[x] == ')'):
+            temp = Token(type=token_types["right_paren"],value=code[x],start_index=x)
+            tokens.append(temp)
+            x = x+1
+            continue
+        elif(code[x] == '{'):
+            temp = Token(type=token_types["left_brace"],value=code[x],start_index=x)
+            tokens.append(temp)
+            x = x+1
+            continue
+        elif(code[x] == '}'):
+            temp = Token(type=token_types["right_brace"],value=code[x],start_index=x)
+            tokens.append(temp)
+            x = x+1
+            continue
+        elif(is_identifier_symbol(code[x])):
+            temp_id = ''
+            y = x
+            while y < len(code) and is_identifier_symbol(code[y]):
+                temp_id += code[y]
+                y+=1
+
+            if temp_id[0] in number_symbols:
+                temp_token = Token(type=token_types['error'],value="Identifiers should not start with numbers!",start_index=x)
+                tokens.append(temp_token)
+                x = y
+                continue
+            else:
+                temp_token = Token(type=token_types['identifier'],value=temp_id,start_index=x)
+                tokens.append(temp_token)
+                x = y
+                continue  
 
         x += 1
 
